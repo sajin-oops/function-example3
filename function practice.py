6e79ded367f95e6522a3e4f91046476797f26d40
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-09 22:53:22 +0530

    Add files via upload

---
+++ function practice.py
@@ -0,0 +1,14 @@
+def create_name(first, last):
+  first = first.capitalize()
+  last = last.capitalize()
+  return first + " " + last
+full_name = create_name("sajin","nijas")
+print(full_name)
+
+'''
+O/P
+ 
+ 
+Sajin Nijas
+
+'''  
